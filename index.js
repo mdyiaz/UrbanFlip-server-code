65187f0eb5aeef20f02ac85f466473693d6db04c
mdyiaz <mdyiaz@gmail.com>
2022-11-08 23:06:54 +0600

    home API

--- index.js
+++ index.js
@@ -22,9 +22,53 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p9sgcbw.mongodb.net/?retryWrites=true&w=majority`;
+
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+async function run () {
+
+    try{
+        const serviceCollection = client.db('assignment-11').collection('services');
+
+        app.get('/services' , async (req, res) => {
+            const id = parseInt(req.query.limit);
+            const query = {}
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.limit(id).toArray();
+            res.send(services);
+        })
+
+    }
+
+
+
+
+
+    finally{
+
+    }
+}
+run().catch(err => console.error(err));
+
+
+
+
+
 
 
 
