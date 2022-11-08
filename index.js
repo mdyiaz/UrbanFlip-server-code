5bcaa8d8ad16a0f951c9b5a07e50c2eb2626f932
mdyiaz <mdyiaz@gmail.com>
2022-11-08 19:54:59 +0600

    basic settings

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('This server is running')
+})
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+    console.log(`Dont Worry your server is running ${port}`);
+})
+
+
+
+
+
+
+require ('dotenv').config();
