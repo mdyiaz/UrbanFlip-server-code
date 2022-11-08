bddade0c717bcce4b6caa2b00c200381da496858
mdyiaz <mdyiaz@gmail.com>
2022-11-08 20:45:53 +0600

    mongoDB setUp

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 5000;
+require ('dotenv').config();
 
 
 
 app.use(cors());
 app.use(express.json());
+
+
+         
+         
+
+
+
+
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.p9sgcbw.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+
+
+
+
 
 
 
@@ -32,4 +59,4 @@ app.listen(port, () => {
 
 
 
-require ('dotenv').config();
+
